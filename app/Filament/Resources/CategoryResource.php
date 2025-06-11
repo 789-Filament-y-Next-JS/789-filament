033c65e2bc8e3e2ac675a4b41b766c39d87c4fda
Mattias Duarte <duartemattias3@gmail.com>
2025-06-11 11:15:58 -0400

    feat: modify category resource

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -3,23 +3,26 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
-use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
-use Filament\Forms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
+    protected static ?string $navigationGroup = 'Menu principal';
+    protected static ?string $navigationLabel = 'Categorias';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
+    
+    protected static ?string $slug = "categorias";
+    protected static ?string $label = "Categoria";
+    protected static ?string $pluralLabel = "Categorias";
+    
     public static function form(Form $form): Form
     {
         return $form
@@ -27,7 +30,12 @@ class CategoryResource extends Resource
                 TextInput::make('name')
                     ->label('Nombre')
                     ->required()
-                    ->placeholder('Ej: Auriculares')
+                    ->placeholder('Ej: Auriculares'),
+                
+                TextInput::make('summary')
+                    ->label('Resumen')
+                    ->required()
+                    ->placeholder('Ej: Auriculares de alta calidad con cancelación de ruido'),
             ]);
     }
 
@@ -35,7 +43,18 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->label('Código'),
+
+                TextColumn::make('name')
+                    ->label('Nombre'),
+                
+                TextColumn::make('summary')
+                    ->label('Resumen'),
+
+                TextColumn::make('created_at')
+                    ->label('F. Registro')
+                    ->date(),
             ])
             ->filters([
                 //
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Category extends Model
 {
@@ -11,4 +13,15 @@ class Category extends Model
         "slug",
         "summary",
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($category){
+            $category->slug = Str::slug($category->name);
+        });
+
+        static::updating(function ($category){
+            $category->slug = Str::slug($category->name);
+        });
+    }
 }
